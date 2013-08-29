30eb2f6f1bd030a921c0b90fda5f8cac50c778ec
Fahv <robertstanday@gmail.com>
2013-08-28 22:26:39 -0700

    Removed the database code because it wasn't needed. Simple file I/O is fine

--- calcore.php
+++ calcore.php
@@ -1,9 +1,15 @@
 <?php
-require_once("src/database_config.php");
+require_once("src/fileIO.php");
 
-//$db->InitDB();
-//$db->closeConnection();
-
-
-
+$file = new FileIO();
+echo "Init: <br />";
+$file->init("/eve/mineralPrices.txt",MODE::ReadPlus);
+echo "Inital Read: ".$file->getContent()." filesize: ".$file->getFileSize()."<br />";
+echo "Write: ";
+echo $file->write("This is a test");
+echo "<br />After Write File Size: ".$file->getFileSize()."<br />";
+$file->read();
+echo "<br /> Read: ".$file->getContent()."<br />";
+$file->close();
+echo "File Closed: <br />";
 ?>
