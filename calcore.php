cf9d84417385bcae18b5ca477cfe10bc3d92c70f
Fahv <robertstanday@gmail.com>
2013-08-29 01:21:36 -0700

    Mineral Prices are not saved -10%

--- calcore.php
+++ calcore.php
@@ -1,15 +1,28 @@
 <?php
 require_once("src/fileIO.php");
 
-$file = new FileIO();
-echo "Init: <br />";
-$file->init("/eve/mineralPrices.txt",MODE::ReadPlus);
-echo "Inital Read: ".$file->getContent()." filesize: ".$file->getFileSize()."<br />";
-echo "Write: ";
-echo $file->write("This is a test");
-echo "<br />After Write File Size: ".$file->getFileSize()."<br />";
-$file->read();
-echo "<br /> Read: ".$file->getContent()."<br />";
-$file->close();
-echo "File Closed: <br />";
+$mineral_prices_file = new FileIO();
+
+$mineral_prices_file->init($_SERVER['DOCUMENT_ROOT']."/eve/mineralPrices.txt",MODE::Read,false);
+
+/** 
+ * Time
+ * Trit 34
+ * Pye 35
+ * Mex 36
+ * Iso 37
+ * Noc 38
+ * Zyd 39
+ * Meg 40
+ * Mor 11399
+ */
+ 
+ $your_array = explode("\n", $mineral_prices_file->getContent());
+ 
+ echo "<pre>";
+ var_dump($your_array);
+ echo "</pre>";
+ 
+ 
+
 ?>
--- src/fileIO.php
+++ src/fileIO.php
@@ -1,4 +1,34 @@
 <?php
+/**
+* A short and simple library to handle file io.
+*
+*
+* The MIT License (MIT)
+*
+* Copyright (c) 2013 Fahv
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+/**
+* Class for the different file open modes.
+*/
 
 final class MODE{
 	const Read = "r";
@@ -13,31 +43,61 @@ final class MODE{
 	}
 }
 
+/**
+* Provides a simple IO interface, using an oop approach.
+*
+*/
+
 class FileIO{
-	private $fileName;
-	private $file;
+	private $requestName;
+	private $handle;
 	private	$fileSize;
-	private $fileContent;
+	private $content;
+	private $url;
 	
-	public function init($fileName,$mode){
-		$this->fileName = $_SERVER['DOCUMENT_ROOT'].$fileName;
-		
-		$this->file = fopen($this->fileName,$mode);
-		if($this->file == false){
-			echo "error opening file";
+	
+	/**
+	 * Opens the file with what ever mode is given.
+	 * 
+	 * $requestName is the request starting with the folder location. Ie "/FileIOLib/test.txt"
+	 * $mode the mode that we want the file to be opened.
+	 * $url if this is for a url or file
+	 * 
+	 * @return nothing if everything works
+	 * @return echos error and dies
+	 * 
+	 */
+	public function init($requestName,$mode,$url){
+		$this->requestName = $requestName;
+		$this->url = $url;
+		$this->handle = fopen($this->requestName,$mode);
+		if($this->handle == false){
+			echo "error opening request";
 			die;
 		}
-		$this->clearCache();
-		$this->fileSize = filesize($this->fileName);
-		
-		$this->fileContent = fread($this->file,$this->fileSize);
+		if($this->url == false){
+			$this->clearCache();
+			$this->fileSize = filesize($this->requestName);
+			
+			if($this->fileSize != 0){
+				$this->content = fread($this->handle,$this->fileSize);
+			}
+		} else{
+			$this->content = stream_get_contents($this->handle);	
+		}
 	}
 	
+	/**
+	 * 
+	 * Reads the whole contents of the file into $fileContent
+	 * 
+	 * @return echos on error
+	 */
 	public function read(){
-		rewind($this->file);
-		if($this->file){
-			$this->fileContent = fread($this->file,$this->fileSize);
-			if($this->fileContent == false)
+		rewind($this->handle);
+		if($this->handle){
+			$this->content = fread($this->handle,$this->fileSize);
+			if($this->content == false)
 			{
 				echo "Error Reading File";
 			}
@@ -46,25 +106,57 @@ class FileIO{
 		}		
 	}
 	
+	/**
+	 * 
+	 * Writes to the file
+	 * 
+	 * $content is the content that is to be written
+	 * 
+	 * @return the number of bytes written
+	 */
 	public function write($content){
-		if($this->file){
-			$written = fwrite($this->file,$content);
+		if($this->handle){
+			$written = fwrite($this->handle,$content);
 			$this->clearCache();
-			$this->fileSize = filesize($this->fileName);
+			$this->fileSize = filesize($this->requestName);
 			return $written;
 		}
 	}
 	
+	/**
+	 * Closes the file handle
+	 */
 	public function close(){
-		fclose($this->file);
+		fclose($this->handle);
 	}
 	
+	/**
+	 * Returns $fileContent
+	 */
 	public function getContent(){
-		return $this->fileContent;
+		return $this->content;
 	}
+	/**
+	 * Returns $fileSize
+	 */
 	public function getFileSize(){
 		return $this->fileSize;
 	}
+	/**
+	 * Returns the position of the file pointer
+	 */
+	 public function getPointer(){
+		 ftell($this->handle);
+	 }
+	 /**
+	 * Sets the position of the file pointer
+	 * Returns 0 upon success -1 otherwise
+	 */
+	 public function setPointer($offset){
+		 fseek($this->handle,$offset);
+	 }
+	
+	
 	private function clearCache(){
 		clearstatcache();
 	}
